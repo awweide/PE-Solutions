2a5ed30d5bc1d0b4257a2fc7b2415dd466ce5fe9
aksel <aksel>
2017-01-22 13:55:21 +0100

    e100.py added

---
+++ e100.py
@@ -0,0 +1,15 @@
+lim = 1000000000
+sqrt = 3+2*(2**0.5)
+
+def calc(x,y):
+	return y*(y+1.0) / (x * (x+1) )
+
+i = 1000000000000	
+while True:
+	j = int(sqrt*i)
+	while calc(i,j) < 2: j+=1
+	if calc(i,j) == 2: break
+	i += 1
+
+print i+1,j+1
+
